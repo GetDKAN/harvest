47e912d13cb963129e21dfef46fc7745614b8c5a
fmizzell <fmizzell.dev@gmail.com>
2020-01-08 09:25:28 -0600

    Fix unittests and code style issues.

--- test/MemStore.php
+++ test/MemStore.php
@@ -7,5 +7,9 @@ use Harvest\Storage\StorageInterface;
 
 class MemStore extends Memory implements StorageInterface
 {
+    public function retrieveAll(): array
+    {
+        return array_keys(parent::retrieveAll());
+    }
 
 }
