ac88808691027d6b09b1f504ae1ac604a0d12bd8
Paul Mitchum <paul@mile23.com>
2025-01-13 14:52:00 -0800

    Sort JSON array before calculating hashes. (#23)

--- src/Util.php
+++ src/Util.php
@@ -4,11 +4,46 @@ namespace Harvest;
 
 class Util
 {
+
+    /**
+     * Generate a hash of a data structure.
+     *
+     * @param mixed $item
+     *   The data structure to hash.
+     *
+     * @return string
+     *   Hash of the serialized data structure.
+     */
     public static function generateHash($item): string
+    {
+        // Encode as JSON and then decode as associative arrays.
+        // @todo Is this the most efficient way to convert an arbitrary
+        //   data structure into an array?
+        $decoded = json_decode(
+            json_encode($item, JSON_THROW_ON_ERROR),
+            true
+        );
+        // Sort the array by keys.
+        static::recursiveKeySort($decoded);
+        return hash('sha256', serialize($decoded));
+    }
+
+    /**
+     * Legacy hash generation.
+     *
+     * We use this for a secondary comparison, if generateHash() says they
+     * don't match.
+     *
+     * @param mixed $item
+     *   The data structure to hash.
+     *
+     * @return string
+     *   Hash of the serialized data structure.
+     */
+    public static function legacyGenerateHash($item): string
     {
         return hash('sha256', serialize($item));
     }
-
 
     public static function getDatasetId($dataset): string
     {
@@ -23,5 +58,27 @@ class Util
             $id = $dataset->identifier;
         }
         return "{$id}";
+    }
+
+    /**
+     * Sort an array in place by key.
+     *
+     * Recursively applies ksort() to any value in the array that is an array.
+     *
+     * @param $array
+     *   The array to be sorted.
+     * @param $flags
+     *   Flags to pass along to ksort().
+     *
+     * @see \ksort()
+     */
+    public static function recursiveKeySort(&$array, int $flags = SORT_REGULAR): void
+    {
+        foreach ($array as &$value) {
+            if (is_array($value)) {
+                static::recursiveKeySort($value);
+            }
+        }
+        ksort($array, $flags);
     }
 }
